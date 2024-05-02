60d26f22e35c9cfa60f02733a8d95201fd4fdbb8
KaterinaDimogiorgi <k.dimogiorgi@gmail.com>
2024-05-02 14:36:59 +0300

    fix cypress config typo

--- cypress.config.js
+++ cypress.config.js
@@ -11,7 +11,7 @@ module.exports = defineConfig({
     "reporter": 'reporters/custom.js',
     "reporterOptions": {
         "configFile": "reporter-config.json"
-    }
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
